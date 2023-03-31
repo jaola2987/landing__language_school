45a025e2570e1fa9bc9d371bfe7b1f45427ae710
jaola2987 <jaola2987@gmail.com>
2023-03-31 23:04:29 +0200

    Added elements of services

--- script/index.js
+++ script/index.js
@@ -1,11 +1,32 @@
-const modalWindow = document.querySelector(".modal__wrapper")
-const modalCloseButton = document.querySelector(".modal__close-button")
-const modalOpenButton = document.querySelector(".modal__open-button")
+import { mock } from "./mock.js"
 
-modalCloseButton.addEventListener("click", function () {
-  modalWindow.classList.remove("open__modal")
-})
+const modalWindow = document.querySelector(".modal__wrapper")
+const modalOpenButton = document.querySelector(".modal__open-button")
+const servicesBox = document.querySelector(".services__container")
 
 modalOpenButton.addEventListener("click", function () {
   modalWindow.classList.add("open__modal")
+})
+
+modalWindow.addEventListener("click", function (event) {
+  if (event.target === modalWindow || event.target.closest(".modal__close-button")) {
+    modalWindow.classList.remove("open__modal")
+  }
+})
+
+mock.forEach((element) => {
+  const serviceWrapper = document.createElement("article")
+  serviceWrapper.classList.add("service__wrapper")
+
+  const serviceTitle = document.createElement("h2")
+  serviceTitle.classList.add("service__title")
+  serviceTitle.textContent = element.title
+  serviceWrapper.append(serviceTitle)
+
+  const serviceSubtitle = document.createElement("p")
+  serviceSubtitle.classList.add("service__subtitle")
+  serviceSubtitle.textContent = element.subtitle
+  serviceWrapper.append(serviceSubtitle)
+
+  servicesBox.append(serviceWrapper)
 })
