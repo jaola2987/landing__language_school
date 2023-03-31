13ada7f6bf59511be31376c62de097d063222ec1
jaola2987 <jaola2987@gmail.com>
2023-03-31 23:29:25 +0200

    Added fetch to submit of form

--- script/index.js
+++ script/index.js
@@ -3,6 +3,7 @@ import { mock } from "./mock.js"
 const modalWindow = document.querySelector(".modal__wrapper")
 const modalOpenButton = document.querySelector(".modal__open-button")
 const servicesBox = document.querySelector(".services__container")
+const form = document.querySelector(".modal__form")
 
 modalOpenButton.addEventListener("click", function () {
   modalWindow.classList.add("open__modal")
@@ -12,6 +13,27 @@ modalWindow.addEventListener("click", function (event) {
   if (event.target === modalWindow || event.target.closest(".modal__close-button")) {
     modalWindow.classList.remove("open__modal")
   }
+})
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  modalWindow.classList.remove("open__modal")
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    name: form.name.value,
+    surname: form.surname.value,
+    phone: form.phone.value,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    form.reset();
+    alert(`Спасибо, ${data.name}, мы с вами свяжемся`)
+  });
 })
 
 mock.forEach((element) => {
