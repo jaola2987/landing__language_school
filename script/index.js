4948e08b16f135b656e1d3e9419c46d03889752b
jaola2987 <jaola2987@gmail.com>
2023-03-31 17:44:28 +0200

    Added modal window

---
+++ script/index.js
@@ -0,0 +1,13 @@
+const modalWindow = document.querySelector(".modal__wrapper")
+const modalCloseButton = document.querySelector(".modal__close-button")
+const modalOpenButton = document.querySelector(".modal__open-button")
+
+modalCloseButton.addEventListener("click", function () {
+  modalWindow.classList.remove("open__modal")
+  console.log(modalWindow)
+})
+
+modalOpenButton.addEventListener("click", function () {
+  modalWindow.classList.add("open__modal")
+  console.log(modalWindow)
+})
